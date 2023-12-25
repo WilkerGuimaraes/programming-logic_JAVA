db8d90864c12bffe2d7834ef1653fe02f014223f
Wilker Guimarães <141461291+WilkerGuimaraes@users.noreply.github.com>
2023-12-25 20:46:12 -0300

    Delete Arquivos_JAVA directory

--- Arquivos_JAVA/LISTA-4/questao_2/CamaroteSuperior.java
+++
@@ -1,7 +0,0 @@
-package questao_2;
-
-public class CamaroteSuperior extends CamaroteInferior{
-	public CamaroteSuperior(double valor, double valorAdicional, String localizacao) {
-		super(valor, valorAdicional, localizacao);
-	}
-}
