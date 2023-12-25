094eb56df58e230ae2faacac9792f9e775fe5d79
Wilker Guimarães <141461291+WilkerGuimaraes@users.noreply.github.com>
2023-12-25 20:46:26 -0300

    Delete ExerciseLists directory

--- ExerciseLists/LIST-2/question_1/PersonTest.java
+++
@@ -1,9 +0,0 @@
-package question_1;
-
-public class PersonTest {
-    public static void main(String[] args) {
-        Person person = new Person("Wilker", 24);
-
-        System.out.println(person.toString());
-    }
-}
